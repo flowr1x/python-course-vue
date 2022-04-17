509940fe3e46aea74c2e19e02471a3956a23b970
Olegeg <bgpfnic@protonmail.com>
2022-04-17 22:15:47 +0300

    add button scroll to top

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,4 +1,5 @@
 <template>
+  <GlobalEvents @scroll="showButtonToTopHide"/>
   <div class="wrapper">
     <Header/>
     <main class="page">
@@ -22,7 +23,7 @@
         <div class="manual__container">
           <div class="manual__title">
             <h2>Содержание </h2>
-            <icon-burger @onClickBurger="getAsideHide"/>
+            <icon-burger id="menu__icon_aside" @onClickBurger="getAsideHide"/>
           </div>
           <div class="manual__content ">
             <aside class="manual__aside" :class="{'manual__aside_hide': !isActive}">
@@ -41,8 +42,13 @@
               <router-view />
             </div>
           </div>
-          <div class="manual__btn-top">
-              <div id="btn-scroll-top">Вернуться на верх</div>
+          <div id="btn-scroll-top" 
+            class="manual__btn-top"
+            :class="{'manual__btn-top_hide': isActiveBtnToTop}"
+            @click="scrollToTop">
+            <svg class="svg-icon" viewBox="0 0 20 20">
+					    <path fill="none" d="M14.989,9.491L6.071,0.537C5.78,0.246,5.308,0.244,5.017,0.535c-0.294,0.29-0.294,0.763-0.003,1.054l8.394,8.428L5.014,18.41c-0.291,0.291-0.291,0.763,0,1.054c0.146,0.146,0.335,0.218,0.527,0.218c0.19,0,0.382-0.073,0.527-0.218l8.918-8.919C15.277,10.254,15.277,9.784,14.989,9.491z"></path>
+					  </svg>
           </div>
         </div>
       </section>
@@ -158,12 +164,27 @@ export default {
                 subtitle: "Кортеж",
                 path: "/theory_19",
             },
-          ]
+      ],
+      offestBtnTop: 400,
+      isActiveBtnToTop: false,
     }
   },
   methods: {
     getAsideHide(isOpen) {
       this.isActive = isOpen;
+    },
+    showButtonToTopHide(event) {
+      if (this.offestBtnTop < window.pageYOffset && !this.isActiveBtnToTop) {
+        this.isActiveBtnToTop = true;
+      } else if (this.offestBtnTop > window.pageYOffset && this.isActiveBtnToTop) {
+        this.isActiveBtnToTop = false;
+      }
+    },
+    scrollToTop() {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -60);
+        setTimeout(this.scrollToTop, 0);
+      } 
     }
   }
 }
