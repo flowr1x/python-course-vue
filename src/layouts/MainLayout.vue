b7693fda38fc01b6e6fb0d8bea73640e487160ec
Olegeg <bgpfnic@protonmail.com>
2022-04-18 23:13:45 +0300

    create file headerTheory

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -61,7 +61,7 @@
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import AsideList from "@/components/AsideList.vue";
-
+import listManualItem from "@/mocks/headerTheory.js"
 export default {
   components: {
     Header, Footer, AsideList
@@ -69,107 +69,7 @@ export default {
   data() {
     return {
       isActive: false,
-      listManualItem: [
-        {
-          title: "Главная страница",
-          path: "/",
-        },
-        { 
-            title: "Глава 1",
-            subtitle: "Краткая история языков программирования",
-            path: "/theory_1",
-        },
-        { 
-            title: "Глава 2",
-            subtitle: "Краткая историческая справка",
-            path: "/theory_2",
-        },
-        { 
-            title: "Глава 3",
-            subtitle: "Типы данных. Переменные",
-            path: "/theory_3",
-        },
-        { 
-            title: "Глава 4",
-            subtitle: "Ввод и вывод данных",
-            path: "/theory_4",
-        },
-        { 
-            title: "Глава 5",
-            subtitle: "Логические выражения и операторы",
-            path: "/theory_5",
-        },
-        { 
-            title: "Глава 6",
-            subtitle: "Ветвление. Условный операторы",
-            path: "/theory_6",
-        },
-        { 
-            title: "Глава 7",
-            subtitle: "Ошибки и исключения. Обработка исключений",
-            path: "/theory_7",
-        },
-        { 
-            title: "Глава 8",
-            subtitle: "Циклы в программировании. Цикл while",
-            path: "/theory_8",
-        },
-        { 
-            title: "Глава 9",
-            subtitle: "Функции в программировании",
-            path: "/theory_9",
-        },
-        { 
-            title: "Глава 10",
-            subtitle: "Локальные и глобальные переменные",
-            path: "/theory_10",
-        },
-        { 
-            title: "Глава 11",
-            subtitle: "Возврат значений из функции. Оператор return",
-            path: "/theory_11",
-        },
-        { 
-            title: "Глава 12",
-            subtitle: "Параметры и аргументы функции",
-            path: "/theory_12",
-        },
-        { 
-            title: "Глава 13",
-            subtitle: "Встроенные функции",
-            path: "/theory_13",
-        },
-        { 
-            title: "Глава 14",
-            subtitle: "Модуль",
-            path: "/theory_14",
-        },
-        { 
-            title: "Глава 15",
-            subtitle: "Генератор псевдослучайных чисел - random",
-            path: "/theory_15",
-        },
-        { 
-            title: "Глава 16",
-            subtitle: "Списки",
-            path: "/theory_16",
-        },
-        { 
-            title: "Глава 17",
-            subtitle: "Цикл for",
-            path: "/theory_17",
-        },
-        { 
-            title: "Глава 18",
-            subtitle: "Строки",
-            path: "/theory_18",
-        },
-        { 
-            title: "Глава 19",
-            subtitle: "Кортеж",
-            path: "/theory_19",
-        },
-      ],
+      listManualItem: listManualItem,
       offestBtnTop: 400,
       isActiveBtnToTop: false,
     }
---
+++ src/mocks/headerTheory.js
@@ -0,0 +1,185 @@
+export default [
+    { 
+        title: "Глава 1",
+        subtitle: "Краткая история языков программирования",
+        path: "/theory_1",
+        shortlinks: [
+            {title: "Программа. Язык программирования", path: "/"},
+            {title: "Основные этапы исторического развития языков программирования", path: "/"},
+            {title: "Разнообразия языков программирования", path: "/"},
+            {title: "Трансляция", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 2",
+        subtitle: "Краткая историческая справка",
+        path: "/theory_2",
+        shortlinks: [
+            {title: "Краткая историческая справка", path: "/"},
+            {title: "Основные особенности языка", path: "/"},
+            {title: "Дзен Питона", path: "/"},
+            {title: "Как писать программы на Python", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 3",
+        subtitle: "Типы данных. Переменные",
+        path: "/theory_3",
+        shortlinks: [
+            {title: "Данные и их типы", path: "/"},
+            {title: "Операции в программировании", path: "/"},
+            {title: "Изменение типов данных", path: "/"},
+            {title: "Переменные", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 4",
+        subtitle: "Ввод и вывод данных",
+        path: "/theory_4",
+        shortlinks: [
+            {title: "Данные и их типы", path: "/"},
+            {title: "Ввод данных. Функция input()", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 5",
+        subtitle: "Логические выражения и операторы",
+        path: "/theory_5",
+        shortlinks: [
+            {title: "Сложные логические выражения", path: "/"},
+            {title: "Операции в программировании", path: "/"},
+            {title: "Изменение типов данных", path: "/"},
+            {title: "Переменные", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 6",
+        subtitle: "Ветвление. Условный операторы",
+        path: "/theory_6",
+        shortlinks: [
+            {title: "Определение", path: "/"},
+            {title: "Условный оператор", path: "/"},
+            {title: "Множественное ветвление: if-elif-else", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 7",
+        subtitle: "Ошибки и исключения. Обработка исключений",
+        path: "/theory_7",
+        shortlinks: [
+            {title: "Ошибки и исключения", path: "/"},
+            {title: "Обработка исключений. Оператор try-except", path: "/"},
+            {title: "Множественное ветвление: if-elif-else", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 8",
+        subtitle: "Циклы в программировании. Цикл while",
+        path: "/theory_8",
+        shortlinks: [
+            {title: "Цикл while", path: "/"},
+            {title: "Обработка исключений. Оператор try-except", path: "/"},
+            {title: "Множественное ветвление: if-elif-else", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 9",
+        subtitle: "Функции в программировании",
+        path: "/theory_9",
+        shortlinks: [
+            {title: "Определение Функции. Оператор def", path: "/"},
+            {title: "Вызов функции", path: "/"},
+            {title: "Функции придают программе структуру", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 10",
+        subtitle: "Локальные и глобальные переменные",
+        path: "/theory_10",
+        shortlinks: [
+            {title: "Возврат нескольких значений", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 11",
+        subtitle: "Возврат значений из функции. Оператор return",
+        path: "/theory_11",
+        shortlinks: [
+            {title: "Произвольное количество аргументов", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 12",
+        subtitle: "Параметры и аргументы функции",
+        path: "/theory_12",
+        shortlinks: [
+            {title: "Функции для работы с символами", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 13",
+        subtitle: "Встроенные функции",
+        path: "/theory_13",
+        shortlinks: [
+            {title: "Ключевое слово Import", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 14",
+        subtitle: "Модуль",
+        path: "/theory_14",
+        shortlinks: [
+            {title: "Генератор псевдослучайных чисел - random", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 15",
+        subtitle: "Генератор псевдослучайных чисел - random",
+        path: "/theory_15",
+        shortlinks: [
+            {title: "Определение", path: "/"},
+            {title: "Функции целых чисел randint() и randrange()", path: "/"},
+            {title: "Функция random() - случайные вещественные числа", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 16",
+        subtitle: "Списки",
+        path: "/theory_16",
+        shortlinks: [
+            {title: "Определение", path: "/"},
+            {title: "Пример: созданеи и заполнение списка", path: "/"},
+            {title: "Функция random() - случайные вещественные числа", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 17",
+        subtitle: "Цикл for",
+        path: "/theory_17",
+        shortlinks: [
+            {title: "Функция range()", path: "/"},
+            {title: "Цикл for и range()", path: "/"},
+            {title: "Функция enumerate()", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 18",
+        subtitle: "Строки",
+        path: "/theory_18",
+        shortlinks: [
+            {title: "Определение", path: "/"},
+            {title: "Методы строк", path: "/"},
+            {title: "Функция enumerate()", path: "/"},
+        ]
+    },
+    { 
+        title: "Глава 19",
+        subtitle: "Кортеж",
+        path: "/theory_19",
+        shortlinks: [
+            {title: "Строки", path: "/"},
+            {title: "Основные команды", path: "/"},
+            {title: "Нарезка строк", path: "/"},
+        ]
+    },
+]
